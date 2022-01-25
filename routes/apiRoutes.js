1f88a545c926fe342845261cf0b379b3bf625677
IanAdamPrice <ianadamprice@gmail.com>
2022-01-25 10:57:20 -0500

    cleaning up the code

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,46 +1,47 @@
 const fs = require('fs');
 const notesArray = require('../db/db.json');
 
-module.exports = function (app) {
+module.exports = (app) => {
+  // create function to get notes then post
+  function updateDb(notes) {
+    notes = JSON.stringify(notes);
+    
+    fs.writeFileSync('./db/db.json', notes, function(err){
+      if (err) {
+        return;
+      }
+    });
+  } // end updateDb function
 
-    function updateDb(notes) {
-        notes = JSON.stringify(notes);
-        fs.writeFileSync('./db/db.json', notes, function(err){
-            if (err) {
-                return console.log(err);
-            }
-        });
+  app.get('/api/notes', (req, res) => {
+    res.json(notesArray);
+  });
+
+  app.post('/api/notes', (req,res) => {
+    if (notesArray.length == 0) {
+      req.body.id = "0";
+    } else {
+      req.body.id = JSON.stringify(JSON.parse(notesArray[notesArray.length - 1].id) + 1);
     }
 
-    app.get('/api/notes', (req, res) => {
-        res.json(notesArray);
-    });
+    notesArray.push(req.body);    
+    updateDb(notesArray);       
+    res.json(req.body);
 
-    app.post('/api/notes', (req,res) => {
-        if (notesArray.length == 0) {
-            req.body.id = "0";
-        } else {
-            req.body.id = JSON.stringify(JSON.parse(notesArray[notesArray.length - 1].id) + 1);
+  }); // end app.post
+
+  app.delete("/api/notes/:id", (req, res) => {
+    let id = req.params.id.toString();
+
+    for (i=0; i < notesArray.length; i++){
+      if (notesArray[i].id == id){
+        res.send(notesArray[i]);
+        notesArray.splice(i,1);
+        break;
         }
+    }
 
-        notesArray.push(req.body);
-        
-        updateDb(notesArray);
+    updateDb(notesArray);
 
-        res.json(req.body);
-    });
-
-    app.delete("/api/notes/:id", function(req, res){
-        let id = req.params.id.toString();
-
-        for (i=0; i < notesArray.length; i++){
-            if (notesArray[i].id == id){
-                res.send(notesArray[i]);
-                notesArray.splice(i,1);
-                break;
-            }
-        }
-
-        updateDb(notesArray);
-    });
+  }); // end app.delete
 };
